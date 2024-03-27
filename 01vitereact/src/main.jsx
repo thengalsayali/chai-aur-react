0894d9ebfd20e6433e75cbba76fde4aff971ef84
Sayali <thengalsayali05@gmail.com>
2024-03-27 23:06:58 +0530

    customreact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+const anotherElemet = (
+  <a href="https://google.com" target="_blank"> Visit google </a>
+)
+  
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
